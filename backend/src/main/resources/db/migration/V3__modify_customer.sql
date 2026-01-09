aca3862a1755928083e553e87a69df99bf5e3dc8
octguy <heyiamthinh1003@gmail.com>
2026-01-09 23:38:55 +0700

    containerize

--- backend/src/main/resources/db/migration/V3__modify_customer.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE customers
-    MODIFY customer_type VARCHAR (255) NOT NULL;
-
-ALTER TABLE accounts
-    MODIFY email VARCHAR (255) NOT NULL;
-
-ALTER TABLE customers
-    MODIFY subscription_plan VARCHAR (255) NOT NULL;
--- backend/src/main/resources/db/migration/V4__adjust.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE accounts
-    DROP COLUMN phone_number;
-
-ALTER TABLE customers
-    MODIFY subscription_plan VARCHAR (255) NOT NULL;
