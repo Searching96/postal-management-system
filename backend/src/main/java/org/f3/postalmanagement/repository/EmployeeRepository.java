fb23f6e5612ae0aed3de04b021ea00cb762c64a5
octguy <heyiamthinh1003@gmail.com>
2026-01-10 23:13:29 +0700

    add EmployeeRepository interface for employee entity management

---
+++ backend/src/main/java/org/f3/postalmanagement/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package org.f3.postalmanagement.repository;
+
+import org.f3.postalmanagement.entity.actor.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
+}
