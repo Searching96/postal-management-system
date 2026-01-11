80422f8de2e697af80ec8827c538e0ee8b5d3374
octguy <heyiamthinh1003@gmail.com>
2026-01-11 20:49:26 +0700

    add endpoints to get Administrative units.

--- backend/src/main/java/org/f3/postalmanagement/repository/ProvinceRepository.java
+++ backend/src/main/java/org/f3/postalmanagement/repository/ProvinceRepository.java
@@ -3,5 +3,9 @@ package org.f3.postalmanagement.repository;
 import org.f3.postalmanagement.entity.administrative.Province;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProvinceRepository extends JpaRepository<Province, String> {
+
+    List<Province> findByAdministrativeRegion_Id(Integer regionId);
 }
--- backend/src/main/java/org/f3/postalmanagement/repository/WardRepository.java
+++ backend/src/main/java/org/f3/postalmanagement/repository/WardRepository.java
@@ -3,5 +3,9 @@ package org.f3.postalmanagement.repository;
 import org.f3.postalmanagement.entity.administrative.Ward;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WardRepository extends JpaRepository<Ward, String> {
+
+    List<Ward> findByProvince_Code(String provinceCode);
 }
